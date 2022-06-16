4bd063ec08d93e5077b56071ab4e3ca12e9c6ada
aralayon <aralayon@theskyplanner.com>
2022-06-16 11:25:15 -0400

    fix  activity file

--- ClientApp/src/components/MyActivity.js
+++ ClientApp/src/components/MyActivity.js
@@ -137,28 +137,6 @@ export default function MyActivity(props){
     return (
         <div  className="shadow p-4 mb-3 gnx-bck-darkgray rounded-4">
             <div>
-<<<<<<< Updated upstream
-                <div className="d-flex justify-content-between">
-                    <div>
-                        <h3 class="font-weight-bold">{(index + 1) + (description == '' 
-                            ? ' Activity' 
-                            : description == 'other'
-                                ? ' Event'
-                                : ' ' + description) }
-                        </h3>
-                        <div className="pl-4">
-                            {response.label}
-                        </div>
-                    </div>
-                    <div>
-                        <button className="ba-button ba-button ba-button-action"
-                                onClick={onRemove}>
-                            <img src={deleteIcon} 
-                                    alt="" 
-                                    width="13"/>
-                        </button>
-                    </div>
-=======
                 <div className="float-right">
                     <button className="ba-button ba-button ba-button-action"
                             onClick={onRemove}>
@@ -166,7 +144,6 @@ export default function MyActivity(props){
                                 alt="" 
                                 width="13"/>
                     </button>
->>>>>>> Stashed changes
                 </div>
             </div>
             <br/>
