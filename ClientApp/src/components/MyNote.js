122568655b5894a7acd81a3218aa3509485aad1d
albermontes <albermontes@gmail.com>
2022-06-22 19:41:42 -0400

    Update MyNote.js

--- ClientApp/src/components/MyNote.js
+++ ClientApp/src/components/MyNote.js
@@ -457,7 +457,7 @@ export default function MyNote(){
                                 </a>
                             </div>
                         </div>
-                        <div className="d-flex justify-content-between p-2 gnx-bck-darkgray border-bottom">
+                        <div className="d-flex justify-content-between p-2 gnx-bck-darkgray">
                             <h3 className="p-3 mb-0">
                                 <a className="ba-home-icon pr-3 pointer"
                                         onClick={onNavigationBack}>
