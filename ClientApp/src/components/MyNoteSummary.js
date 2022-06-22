cae3c44c00cbc2b437ab089fab7f1e3b41ce51cb
albermontes <albermontes@gmail.com>
2022-06-22 19:38:35 -0400

    adjustments

--- ClientApp/src/components/MyNoteSummary.js
+++ ClientApp/src/components/MyNoteSummary.js
@@ -148,7 +148,7 @@ export default function MyNoteSummary(props){
                                     </button>
                                 </div>  
                             </div>
-                            <div className="ba-note p-2 gnx-color-lightgray">
+                            <div className="ba-note p-2 gnx-color-lightgray"> 
                                 {x.summary}
                             </div>   
                         </div>
